b5de03d6931b671181df40d66626fe00abc0152e
Adam M <adammilsom@gmail.com>
2019-11-20 15:07:47 +0000

    new things

---
+++ pythonstuff/inputoutput.py
@@ -0,0 +1,8 @@
+# input and output exercise
+
+with open("/home/user01/ncas-isc/my-isc-work/pythonstuff/weather.csv") as reader :
+    line = reader.readline()
+    print(line)
+
+print("it's over")
+
